d72268411bf94c510f209b13246b9c47ad188555
VladiBern <vladibern@gmail.com>
2018-01-01 19:44:32 +0000

    Update DataTypes.java

--- DataTypes.java
+++ DataTypes.java
@@ -40,83 +40,41 @@ Objective
 
         You will not pass this challenge if you attempt to assign the Sample Case values to your variables instead of following the instructions above and reading input from stdin.
 
-        Submissions:241005
-        Max Score:30
-        Difficulty: Easy
-        Rate This Challenge:
+     
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
-        More
-        Current Buffer (saved locally, editable)
-        Java 8
-
-
-        1
-        import ↔;
-        6
-        ​
-        7
 public class Solution {
-8
-
-        9
+	
     public static void main(String[] args) {
-        10
         int i = 4;
-        11
         double d = 4.0;
-        12
         String s = "HackerRank ";
-        13
-
-        14
+		
         Scanner scan = new Scanner(System.in);
+             /* Declare second integer, double, and String variables. */
+         int a  = scan.nextInt();
+         double b = scan.nextDouble();
+        
 
-        15
-        /* Declare second integer, double, and String variables. */
-        16
-        int a  = scan.nextInt();
-        17
-        double b = scan.nextDouble();
-        18
-
-        19
-​
-        20
-        scan.nextLine();
-        21
-        String c = scan.nextLine();
-        22
+         scan.nextLine();
+         String c = scan.nextLine();
                 /* Read and save an integer, double, and String to your variables.*/
-        23
         // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
-        24
         System.out.println(i + a);
-        25
         System.out.println(d + b);
-        26
         System.out.println(s + c);
-        27
-​
-        28
 
-        29
+      
         /* Print the sum of both integer variables on a new line. */
-        30
-​
-        31
+
         /* Print the sum of the double variables on a new line. */
-        32
-
-        33
+		
         /* Concatenate and print the String variables on a new line; 
-34
-            the 's' variable above should be printed first. */
-        35
-​
-
-        36
+        	the 's' variable above should be printed first. */
         scan.close();
-        37
     }
-38
 }
