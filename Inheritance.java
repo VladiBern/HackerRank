6321cb12c93cc6f4d0114fea9dd33238ff4ee8e1
VladiBern <vladibern@gmail.com>
2018-01-01 19:58:25 +0000

    Create Inheritance.java

---
+++ Inheritance.java
@@ -0,0 +1,85 @@
+Task 
+You are given two classes, Person and Student, where Person is the base class and Student is the derived class. Completed code for Person and a declaration for Student are provided for you in the editor. Observe that Student inherits all the properties of Person.
+
+Complete the Student class by writing the following:
+
+A Student class constructor, which has  parameters:
+A string, FIRST NAME.
+A string, LAST NAME.
+An integer, ID .
+An integer array (or vector) of test scores, SCORES .
+
+Sample Input
+
+Heraldo Memelli 8135627
+2
+100 80
+Sample Output
+
+ Name: Memelli, Heraldo
+ ID: 8135627
+ Grade: O
+ 
+ ---------------
+ 
+ import java.util.*;
+
+class Person {
+	protected String firstName;
+	protected String lastName;
+	protected int idNumber;
+	
+	// Constructor
+	Person(String firstName, String lastName, int identification){
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.idNumber = identification;
+	}
+	
+	// Print person data
+	public void printPerson(){
+		 System.out.println(
+				"Name: " + lastName + ", " + firstName 
+			+ 	"\nID: " + idNumber); 
+	}
+	 
+}
+
+class Student extends Person{
+	private int[] testScores;
+
+    /*	
+    *   Class Constructor
+    *   
+    *   @param firstName - A string denoting the Person's first name.
+    *   @param lastName - A string denoting the Person's last name.
+    *   @param id - An integer denoting the Person's ID number.
+    *   @param scores - An array of integers denoting the Person's test scores.
+    */
+    // Write your constructor here
+
+    /*	
+    *   Method Name: calculate
+    *   @return A character denoting the grade.
+    */
+    // Write your method here
+}
+
+class Solution {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String firstName = scan.next();
+		String lastName = scan.next();
+		int id = scan.nextInt();
+		int numScores = scan.nextInt();
+		int[] testScores = new int[numScores];
+		for(int i = 0; i < numScores; i++){
+			testScores[i] = scan.nextInt();
+		}
+		scan.close();
+		
+		Student s = new Student(firstName, lastName, id, testScores);
+		s.printPerson();
+		System.out.println("Grade: " + s.calculate() );
+	}
+}
